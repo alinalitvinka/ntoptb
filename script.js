fd4b94610b2dde0dbd09ced627fe819f36f49ccf
alinalitvinka <36643630+alinalitvinka@users.noreply.github.com>
2022-04-11 15:11:02 +0300

    feat: add zoom plugin for otzyvy

--- script.js
+++ script.js
@@ -32,3 +32,30 @@ const swiperOtzyvy = new Swiper('.otzyvy_slider', {
     clickable: true,
   },
 });
+
+$(document).ready(function() {
+	$('.otzyvy_slider').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
